022eb73ccd74c5ae6e1d2457d0b99ce472d4cdd0
Miguel Garcia <2185056+miguelgherrera@users.noreply.github.com>
2026-01-09 20:42:12 +0100

    chore: upgrade AWS provider to >= 6.28.0 and required_version >= 1.11.1

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.11.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 6.28.0"
     }
   }
 }
